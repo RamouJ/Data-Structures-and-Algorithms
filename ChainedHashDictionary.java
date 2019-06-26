49d45fbce682d1d366ea2671979356ea1b8ab8bc
RamouJ <ramoujallow@rocketmail.com>
2019-06-25 22:38:15 -0700

    Update ChainedHashDictionary.java

--- ChainedHashDictionary.java
+++ ChainedHashDictionary.java
@@ -5,20 +5,11 @@ import misc.exceptions.NoSuchKeyException;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
-/**
- * @see IDictionary and the assignment page for more details on what each method should do
- */
 public class ChainedHashDictionary<K, V> implements IDictionary<K, V> {
     private final double lambda;
-
-    // You MUST use this field to store the contents of your dictionary.
-    // You may not change or rename this field: we will be inspecting
-    // it using our private tests.
     private IDictionary<K, V>[] chains;
     private int size;
     private int tableSize;
-
-    // You're encouraged to add extra fields (and helper methods) though!
 
     public ChainedHashDictionary() {
         this.size = 0;
@@ -35,17 +26,8 @@ public class ChainedHashDictionary<K, V> implements IDictionary<K, V> {
 
     }
 
-    /**
-     * This method will return a new, empty array of the given size
-     * that can contain IDictionary<K, V> objects.
-     * <p>
-     * Note that each element in the array will initially be null.
-     */
     @SuppressWarnings("unchecked")
     private IDictionary<K, V>[] makeArrayOfChains(int arraySize) {
-        // Note: You do not need to modify this method.
-        // See ArrayDictionary's makeArrayOfPairs(...) method for
-        // more background on why we need this method.
         return (IDictionary<K, V>[]) new IDictionary[arraySize];
     }
 
@@ -174,48 +156,6 @@ public class ChainedHashDictionary<K, V> implements IDictionary<K, V> {
         return new ChainedIterator<>(this.chains);
     }
 
-    /**
-     * Hints:
-     * <p>
-     * 1. You should add extra fields to keep track of your iteration
-     * state. You can add as many fields as you want. If it helps,
-     * our reference implementation uses three (including the one we
-     * gave you).
-     * <p>
-     * 2. Before you try and write code, try designing an algorithm
-     * using pencil and paper and run through a few examples by hand.
-     * <p>
-     * We STRONGLY recommend you spend some time doing this before
-     * coding. Getting the invariants correct can be tricky, and
-     * running through your proposed algorithm using pencil and
-     * paper is a good way of helping you iron them out.
-     * <p>
-     * 3. Think about what exactly your *invariants* are. As a
-     * reminder, an *invariant* is something that must *always* be
-     * true once the constructor is done setting up the class AND
-     * must *always* be true both before and after you call any
-     * method in your class.
-     * <p>
-     * Once you've decided, write them down in a comment somewhere to
-     * help you remember.
-     * <p>
-     * You may also find it useful to write a helper method that checks
-     * your invariants and throws an exception if they're violated.
-     * You can then call this helper method at the start and end of each
-     * method if you're running into issues while debugging.
-     * <p>
-     * (Be sure to delete this method once your iterator is fully working.)
-     * <p>
-     * Implementation restrictions:
-     * <p>
-     * 1. You **MAY NOT** create any new data structures. Iterators
-     * are meant to be lightweight and so should not be copying
-     * the data contained in your dictionary to some other data
-     * structure.
-     * <p>
-     * 2. You **MAY** call the `.iterator()` method on each IDictionary
-     * instance inside your 'chains' array, however.
-     */
     private static class ChainedIterator<K, V> implements Iterator<KVPair<K, V>> {
         private IDictionary<K, V>[] chains;
         int size;
@@ -235,14 +175,6 @@ public class ChainedHashDictionary<K, V> implements IDictionary<K, V> {
                     break;
                 }
             }
-
-            // if (itr == null) {
-            //     throw new NoSuchElementException();
-            // }
-            // while (chains[i] == null) {
-            //     i++;
-            // }
-            // this.itr = chains[i].iterator();
         }
 
         @Override
