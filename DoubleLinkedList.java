b5b1bfae909a234ef630025c25937b01359970be
RamouJ <ramoujallow@rocketmail.com>
2019-06-25 22:28:44 -0700

    Update DoubleLinkedList.java

--- DoubleLinkedList.java
+++ DoubleLinkedList.java
@@ -1,22 +1,11 @@
 package datastructures.concrete;
 
-//import com.sun.java.util.jar.pack.ConstantPool;
-
 import datastructures.interfaces.IList;
 import misc.exceptions.EmptyContainerException;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
-/**
- * Note: For more info on the expected behavior of your methods:
- *
- * @see datastructures.interfaces.IList
- * (You should be able to control/command+click "IList" above to open the file from IntelliJ.)
- */
 public class DoubleLinkedList<T> implements IList<T> {
-    // You may not rename these fields or change their types.
-    // We will be inspecting these in our private tests.
-    // You also may not add any additional fields.
     private Node<T> front;
     private Node<T> back;
     private int size;
@@ -255,16 +244,13 @@ public class DoubleLinkedList<T> implements IList<T> {
 
     @Override
     public boolean contains(T other) {
-        //Node nodeToCompare = new Node(other);
         Node<T> current = front;
 
         while (current != null) {
-            //Passes null element test
             if (current.data == null & other == null) {
                 return true;
 
             }
-            //Passes index of and contains correctly compares of item
             if (current.data != null && current.data.equals(other)) {
                 return true;
             }
@@ -275,15 +261,10 @@ public class DoubleLinkedList<T> implements IList<T> {
 
     @Override
     public Iterator<T> iterator() {
-        // Note: we have provided a part of the implementation of
-        // an iterator for you. You should complete the methods stubs
-        // in the DoubleLinkedListIterator inner class at the bottom
-        // of this file. You do not need to change this method.
         return new DoubleLinkedListIterator<>(this.front);
     }
 
     private static class Node<E> {
-        // You may not change the fields in this node or add any new fields.
         public final E data;
         public Node<E> prev;
         public Node<E> next;
@@ -298,22 +279,14 @@ public class DoubleLinkedList<T> implements IList<T> {
             this(null, data, null);
         }
 
-        // Feel free to add additional constructors or methods to this class.
     }
 
     private static class DoubleLinkedListIterator<T> implements Iterator<T> {
-        // You should not need to change this field, or add any new fields.
         private Node<T> current;
-
         public DoubleLinkedListIterator(Node<T> current) {
-            // You do not need to make any changes to this constructor.
             this.current = current;
         }
-
-        /**
-         * Returns 'true' if the iterator still has elements to look at;
-         * returns 'false' otherwise.
-         */
+        
         public boolean hasNext() {
             if (current != null) {
                 return true;
@@ -321,13 +294,6 @@ public class DoubleLinkedList<T> implements IList<T> {
             return false;
         }
 
-        /**
-         * Returns the next item in the iteration and internally updates the
-         * iterator to advance one element forward.
-         *
-         * @throws NoSuchElementException if we have reached the end of the iteration and
-         *                                there are no more elements to look at.
-         */
         public T next() {
             Node<T> pointer = current;
             if (hasNext()) {
